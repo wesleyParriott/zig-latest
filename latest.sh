4285acb2686d862a6405e115e12be3981be7637f
vvesley <wesleyprrtt@gmail.com>
2023-04-19 15:21:14 -0500

    manual version control

--- latest.sh
+++ latest.sh
@@ -7,14 +7,13 @@ tar -xvf zig.tar.xz && mv zig-* zig
 
 # we do a little manual backup management here in case folks want to rollback to the previous version 
 if test -d '/usr/local/share/zig'; then
-    HASH=$(date | sha256sum)
-    TEMPPATH="/usr/local/share/$HASH"
+    TEMPPATH="/usr/local/share/.tmp"
     BACKUPPATH='/usr/local/share/zig~'
 
     sudo mv '/usr/local/share/zig' $TEMPPATH
 
     if test -d $BACKUPPATH; then
-        rm -r $BACKUPPATH
+        sudo rm -r $BACKUPPATH
     fi
 
     sudo mv $TEMPPATH $BACKUPPATH
